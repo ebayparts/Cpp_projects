f707f089f19e76b2c25e400dd907db76d87a5c54
ebayparts <sanyav89@ukr.net>
2020-10-07 20:04:11 +0300

    string length in function

--- countSymbolsInString/countSymbolsInString/Source.cpp
+++ countSymbolsInString/countSymbolsInString/Source.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int stringLength(string str, int countSym) {
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		countSym++;
+	}
+	return countSym;
+}
+
 int main() {
 	string str;
 	cout << "Type your string:" << endl;
 	getline(cin, str);
-	int count = 0;
-	for (size_t i = 0; str[i] != '\0'; i++)
-	{
-		count++;
-	}
+	int count=0;
+	count = stringLength(str, count);
 	cout << "Your string have " << count << " symbols" << endl;
 	return 0;
 }
